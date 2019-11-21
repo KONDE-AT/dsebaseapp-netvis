2f9e8a6a31d7156cdedd85636d9a27c07002d356
csae8092 <peter.andorfer@gmail.com>
2019-11-21 13:14:38 +0100

    fix in js thanks to @stefanprobst

--- netvis.js
+++ netvis.js
@@ -1,7 +1,32 @@
 function showGraph(viscontainer, id, type) {
-  const sourceurl = `entity-as-graph.xql?id=${id}&type=${type}`;
+  const sourceUrl = `entity-as-graph.xql?id=${id}&type=${type}`;
   const visCard = document.getElementById(viscontainer);
   var spinner = new Spinner().spin();
+
+  const toArray = prop => Array.isArray(prop) ? prop : [prop]
+
+  const fetchData = url => {
+    visCard.appendChild(spinner.el);
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(graph => {
+        visCard.removeChild(spinner.el);
+
+        return {
+          nodes: toArray(graph.nodes || []),
+          edges: toArray(graph.edges || []),
+          types: {
+            nodes: toArray(graph.types.nodes || []),
+            edges: toArray(graph.types.edges || []),
+          }
+        }
+      })
+      .catch(error => {
+        visCard.removeChild(spinner.el);
+        console.error("Error fetching graph", error)
+      })
+  }
 
   const render = graph => {
     ReactDOM.render(
@@ -15,49 +40,21 @@ function showGraph(viscontainer, id, type) {
         onNodeClick: ({
           node
         }) => {
-          console.log(node);
           if (!node) {
             console.error("No node found");
             return;
           }
           const url = node.as_graph;
           if (!url) {
-            console.error("No URL found");
+            console.error("No URL specified");
             return;
           }
-          visCard.appendChild(spinner.el);
-          fetch(url)
-            .then(response => response.json())
-            .then(graph => {
-              visCard.removeChild(spinner.el);
-              return render(graph)
-            })
-            .catch(error => {
-              console.error(error)
-            });
+          fetchData(url).then(render)
         }
       }),
       document.getElementById(viscontainer)
     );
   };
 
-  visCard.appendChild(spinner.el);
-  fetch(sourceurl)
-    .then(response => response.json())
-    .then(graph => {
-      const toArray = prop => Array.isArray(prop) ? prop : [prop]
-      return {
-        nodes: toArray(graph.nodes || []),
-        edges: toArray(graph.edges || []),
-        types: {
-          nodes: toArray(graph.types.nodes || []),
-          edges: toArray(graph.types.edges || []),
-        }
-      }
-    })
-    .then(render)
-    .catch(error => {
-      // handle your errors here
-      console.error(error);
-    });
+  fetchData(sourceUrl).then(render)
 }
